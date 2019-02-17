d379180ac531e347d17616d93aa0a5be903df941
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2019-02-16 22:23:17 -0500

    added sobel and pixelate
    added 2 functions: applySobel() and applyPixelation()

--- pictures.js
+++ pictures.js
@@ -181,3 +181,115 @@ function applyContrast(inputImageData, outputImageData) {
 	}
 	return outputImageData;	
 }
+
+
+// this one is interesting. i like it. but it needs an 'edge thinner'
+function applySobel(inputImageData, outputImageData) {
+	
+	// sorbel on compterphile:  https://www.youtube.com/watch?v=uihBwtPIBxM
+
+	// sobel on wikipedia:			https://en.wikipedia.org/wiki/Sobel_operator
+	
+	var w = inputImageData.width;
+	var h = inputImageData.height;
+		
+	var matrixSize = 3;		
+	var Gx = [[-1, 0, 1],[-2, 0, 2],[-1, 0, 1]];
+	var Gy = [[-1, -2, -1],[0, 0, 0],[1, 2, 1]];
+	var offset = Math.floor(matrixSize/2);
+
+
+	for (var i = 0; i < inputImageData.data.length; i+=4) {
+	
+		// for each element, get the x-y coordinates
+		var pixel = Math.floor(i/4);
+		var y = Math.floor(pixel/w);
+		var x = pixel - y*w;
+		
+		var gx = 0;
+		var gy = 0;
+		var G = 0;
+		
+		// loop over the surrounding pixels
+		for (var j = 0; j < matrixSize; j++) {
+			for (var k = 0; k < matrixSize; k++) {
+      
+				// i need to offset x and y
+				var x1 = x + j - offset;
+				x1 = Math.min(Math.max(x1, 0), w-1);  // constrain x1 between 0 and w
+				var y1 = y + k - offset;
+				y1 = Math.min(Math.max(y1, 0), h-1);
+				var loc = x1 + y1*w;
+				loc = Math.min(Math.max(loc, 0), w*h-1);
+				
+				let r = inputImageData.data[4*loc+0];
+				let g = inputImageData.data[4*loc+1];
+				let b = inputImageData.data[4*loc+2];
+			
+				let intensity = 0.2126*r + 0.7152*g + 0.0722*b;
+				
+				gx += intensity*Gx[j][k];
+				gy += intensity*Gy[j][k];
+				
+			}
+		}
+		
+		G = Math.sqrt(Math.pow(gx, 2) + Math.pow(gy, 2));
+		
+		// write over the elements
+		outputImageData.data[i+0] = G;  
+		outputImageData.data[i+1] = G;
+		outputImageData.data[i+2] = G;
+		outputImageData.data[i+3] = 255;
+	}
+	return outputImageData;	
+}
+
+// there has to be a better way for me to do this; and there are some quirks, ie. the edges
+function applyPixelation(inputImageData, outputImageData) {
+	
+	var w = inputImageData.width;
+	var h = inputImageData.height;
+	
+	var s = 15;
+	
+	// easier to start with x-y in this one
+	for (var j = 0; j < h; j += s) {
+		for (var i = 0; i < w; i += s) {
+			
+			var rTotal = 0;
+			var gTotal = 0;
+			var bTotal = 0;
+			
+			for (var k = 0; k < s; k++) {
+				for (var f = 0; f < s; f++) {
+					var x = i + k;  // x-y is the position of this pixel relative to the image
+					var y = j + f;
+					var pixel = x + y*w;  // pixel is the pixels number
+					var loc = 4*pixel;
+					
+					rTotal = inputImageData.data[loc+0];
+					gTotal = inputImageData.data[loc+1];
+					bTotal = inputImageData.data[loc+2];
+					
+				}
+			}
+			
+			for (var k = 0; k < s; k++) {
+				for (var f = 0; f < s; f++) {
+					var x = i + k;  // x-y is the position of this pixel relative to the image
+					var y = j + f;
+					var pixel = x + y*w;  // pixel is the pixels number
+					var loc = 4*pixel;
+					
+					outputImageData.data[loc+0] = rTotal;
+					outputImageData.data[loc+1] = gTotal;
+					outputImageData.data[loc+2] = bTotal;
+					
+				}
+			}			
+			
+		}
+	}
+	return outputImageData;	
+}
