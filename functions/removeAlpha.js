7601f081c298cb37405d82f9d2841849de721c24
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2019-12-19 01:22:30 -0500

    Update removeAlpha.js

--- functions/removeAlpha.js
+++ functions/removeAlpha.js
@@ -1,18 +1,25 @@
-function removeAlpha(inputImageData) {
+function removeAlpha(inputImageData, t) {
 
 	var outputImageData = copyImageData(inputImageData)
 	
-	let r = outputImageData.data[i+0];
-	let g = outputImageData.data[i+1];
-	let b = outputImageData.data[i+2];
-	
 	for (var i = 0; i < inputImageData.data.length; i+=4) {
-		if (r === 255 && g === 255 && b === 255) {
+	
+		let r = inputImageData.data[i+0];
+		let g = inputImageData.data[i+1];
+		let b = inputImageData.data[i+2];
+	
+		if (r >= t && g >= t && b >=t) {
 			outputImageData.data[i+0] = r;
 			outputImageData.data[i+1] = g;
 			outputImageData.data[i+2] = b;
-			outputImageData.data[i+3] = 0;
+			outputImageData.data[i+3] = 120;
 		}
 	}
 	return outputImageData;
 }
+
+/*
+
+ctx.putImageData(removeAlpha(workingImageData, 100), 0, 0);
+		
+*/
